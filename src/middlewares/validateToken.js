f0b2cad409aad090ab5496dc2763f4180fa16420
CarolinaAriadne <c.ariadneadv@gmail.com>
2022-11-07 21:51:14 -0300

    midd-validateToken

---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+const verifyToken = async (req, res, next) => {
+  const token = req.header("authorization");
+  if (!token) {
+    return res.status(401).json({ message: "Token not found" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Expired or invalid token" });
+  }
+};
+
+module.exports = {
+  verifyToken,
+};
+
+// http://blog.thiagobelem.net/relacionamento-de-tabelas-no-mysql
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const { verifyToken } = require("../middlewares/validateToken");
+
 const {
   getAllUsers,
   getUserById,
@@ -8,8 +10,8 @@ const {
   createUser,
 } = require("../controllers/controllerUser");
 
-router.get("/users", getAllUsers);
-router.get("/users/:user_id", getUserById);
+router.get("/users", verifyToken, getAllUsers);
+router.get("/users/:user_id", verifyToken, getUserById);
 router.post("/login", loginUser);
 router.post("/users", createUser);
 
@@ -21,13 +23,12 @@ const {
   deleteTask,
 } = require("../controllers/controller");
 
-router.get("/tasks", getAllTasks);
-router.get("/tasks/:id", getTaskById);
-router.post("/tasks", createTask);
-router.put("/tasks/:id", updateTask);
-router.delete("/tasks/:id", deleteTask);
+router.get("/tasks", verifyToken, getAllTasks);
+router.get("/tasks/:id", verifyToken, getTaskById);
+router.post("/tasks", verifyToken, createTask);
+router.put("/tasks/:id", verifyToken, updateTask);
+router.delete("/tasks/:id", verifyToken, deleteTask);
 
 module.exports = router;
-
 
 // joao@joao.com - joãosilva

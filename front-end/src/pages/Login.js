77851bca43271c1a43d2602e2940b08e31f589e7
CarolinaAriadne <c.ariadneadv@gmail.com>
2022-12-22 20:01:50 -0300

    page-login-incomplete

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,11 +1,65 @@
-import {useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import api from '../services/api';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../services/api";
 // import { AppContext } from '../contexts/AppContext';
 
-
 export default function LoginPage() {
-    const [error, setError] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [disabled, setDisabled] = useState(true);
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await api.post("/login", { email, password });
+      if (data.token) {
+        localStorage.setItem("user", JSON.stringify(data));
+        navigate("/tasks");
+      }
+    } catch (err) {
+      setError("Dados inválidos");
+    }
+  };
+
+  const disableSubmit = () => {
+    if (typeof email === "string" && typeof password === "string") {
+      setDisabled(false);
+      setError("");
+    } else {
+      setDisabled(true);
+      setDisabled("Dados inválidos");
+    }
+  };
+
+  return (
+      <form onSubmit={ onSubmit }>
+          <div>
+              <label htmlFor="email">
+                  Login
+                  <input
+                  name="email"
+                  type="text"
+                  onChange={( { target } ) => setEmail(target.value)}
+                  onKeyUp={ disableSubmit }
+                  value= { email }
+                  />
+              </label>
+          </div>
+          <div>
+              <label htmlFor="password">
+                  Senha
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={ ({target}) => setPassword(target.value) }
+                    onKeyUp={ disableSubmit }
+                    value={ password }
+                  />
+              </label>
+          </div>
+      </form>
+  )
 }
